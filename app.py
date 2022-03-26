710bbdb893e4196bdf67e828ad4efdc1bda5be43
LeQA <lequanganh97@gmail.com>
2022-03-26 17:24:37 +0300

    Update minor detail on index and profile, resolve Nonetype error in app

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template ,redirect, url_for, request, make_response,flash,url_for,send_from_directory
 from pymongo import MongoClient
 from werkzeug.utils import secure_filename
+from functools import wraps
+import string 
+import random
 import os 
 
 UPLOAD_FOLDER = './upload'
@@ -12,12 +15,35 @@ client = MongoClient("mongodb://localhost:27017/?readPreference=primary&appname=
 db = client['web_course']
 collection = db['practice']
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+S=10
+
+def is_authenticated(request):
+    username = request.cookies.get('username', '')
+    token = request.cookies.get('token', '')
+    db_username = collection.find_one({'username':username})
+    if token:
+        if username == db_username['username']:
+            token == db_username['token']
+            return True
+        return False
+    return False
+
+def login_required(func):
+    @wraps(func)
+    def verify(*args, **kwargs):
+        if is_authenticated(request):
+            return func(*args, **kwargs)
+        else:
+            flash('Please login first!!!','error')
+            return redirect(url_for('login'))
+    return verify
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/profile/')
+@login_required
 def profile():
     return render_template('profile.html')
 
@@ -27,10 +53,18 @@ def login():
         username = request.form.get('username', '')
         password = request.form.get('password', '')
         user = collection.find_one({'username':request.form['username']})
-        if username == user['username']:
-            if password == user['password']:
-                response = make_response(redirect(url_for('profile')))
-                return response
+        if user != None:
+            if username == user['username']:
+                if password == user['password']:
+                    random_string = ''.join(random.choices(string.ascii_uppercase + string.digits, k = S)) 
+                    session_token = random_string
+                    response = make_response(redirect(url_for('profile')))
+                    response.set_cookie(key='username',value=username)
+                    response.set_cookie(key='token',value=session_token)
+                    collection.update_one({'username':username},{'$set':{'token':session_token}})
+                    return response
+                else:
+                    flash('Username or password is invalid', 'error') 
             else:
                 flash('Username or password is invalid', 'error')
         else:
@@ -75,6 +109,16 @@ def upload():
         else:
             return 'invalid file'
     return render_template('upload.html')
+
+@app.route('/signout', methods=['GET', 'POST'])
+@login_required
+def signout():
+    username = request.cookies.get('username', '')
+    collection.update_one({'username':username},{'$set':{'token':' '}})
+    response = make_response(redirect(url_for('login')))
+    response.set_cookie(key='token',value='')
+    flash('Sign Out successfully!', 'message')
+    return response
 
 @app.route('/uploaded/<filename>')
 def uploaded(filename):
